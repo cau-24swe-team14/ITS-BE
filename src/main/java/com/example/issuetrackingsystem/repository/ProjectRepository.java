34241eea0f19dea26d73ababd5a06e18c3c0024c
tthisag246 <tthisag246@gmail.com>
2024-05-31 01:08:14 +0900

    Fix: ProjectRepository @Param

--- src/main/java/com/example/issuetrackingsystem/repository/ProjectRepository.java
+++ src/main/java/com/example/issuetrackingsystem/repository/ProjectRepository.java
@@ -4,8 +4,9 @@ import com.example.issuetrackingsystem.domain.Project;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
     @Query("SELECT p FROM Project p JOIN ProjectAccount pa ON p.projectId = pa.project.projectId WHERE pa.account.accountId = :accountId")
-    List<Project> findByAccountId(Long accountId);
+    List<Project> findByAccountId(@Param("accountId") Long accountId);
 }
