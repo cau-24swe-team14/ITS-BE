2ab3e503e5ea201a47151ed52f7233703e7079ea
tthisag246 <tthisag246@gmail.com>
2024-05-31 03:26:31 +0900

    Cors 및 Credentials 설정

--- src/main/java/com/example/issuetrackingsystem/Config/SecurityConfig.java
+++ src/main/java/com/example/issuetrackingsystem/Config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.example.issuetrackingsystem.Config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +12,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -19,9 +23,21 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests((authz) -> authz
-                .anyRequest().permitAll()
-            )
+            .authorizeHttpRequests((requests) -> requests
+                .anyRequest().permitAll())
+            .cors(corsCustomizer -> corsCustomizer.configurationSource(
+                new CorsConfigurationSource() {
+                @Override
+                public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+                    config.setAllowedMethods(Collections.singletonList("*"));
+                    config.setAllowCredentials(true);
+                    config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setMaxAge(3600L); //1시간
+                    return config;
+                }
+            }))
             .httpBasic(withDefaults());
         return http.build();
     }
